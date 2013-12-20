8a74a7d1cd6ed39db01ee6dbfab03adbb558cff7
ckagiri <charleskagiri@gmail.com>
2013-12-20 09:46:55 +0300

    added web admin project

---
+++ src/app/AsianLines.Web.Admin/App_Start/BundleConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Optimization;
+
+namespace AsianLines.Web.Admin
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+        }
+    }
+}
---
+++ src/app/AsianLines.Web.Admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AsianLines.Web.Admin.WebApiApplication" Language="C#" %>
