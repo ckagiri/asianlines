a2cb67cdca76442455982b4b0ebe3621c4d51707
ckagiri <charleskagiri@gmail.com>
2013-12-20 14:53:41 +0300

    added league season fixture models

---
+++ src/app/AsianLines.Core/Model/MatchOdds.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AsianLines.Core.Model
+{
+    public class MatchOdds
+    {
+        public MatchOdds()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; private set; }
+        public Guid FixtureId { get; set; }
+        public OddsType OddsType { get; set; }
+        public decimal Handicap { get; set; }
+    }
+
+    public enum OddsType
+    {
+        None,
+        AsianHandicap,
+        AsianGoals
+    }
+}
---
+++ src/app/AsianLines.Core/Model/Season.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace AsianLines.Core.Model
+{
+    public class Season
+    {
+        public Season()
+        {
+            Teams = new Collection<Team>();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Guid LeagueId { get; set; }
+        public League League { get; set; }
+
+        public ICollection<Team> Teams { get; set; }
+    }
+}
+
