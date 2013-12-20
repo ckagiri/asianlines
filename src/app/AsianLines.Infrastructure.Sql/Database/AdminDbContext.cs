566a0884210850f2a4b86a91496cc2b8d2f36ae1
ckagiri <charleskagiri@gmail.com>
2013-12-20 09:09:13 +0300

    team-model and console app for db initialization

---
+++ src/app/AsianLines.Infrastructure.Sql/Database/AdminDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using AsianLines.Core.Model;
+
+namespace AsianLines.Infrastructure.Sql.Database
+{
+    public class AdminDbContext : DbContext
+    {
+        public AdminDbContext() : base(nameOrConnectionString: "AsianLines")
+        { }
+
+        public AdminDbContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        { }
+
+        public DbSet<Team> Teams { get; set; }
+    }
+}
---
+++ src/app/AsianLines.TestConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using AsianLines.Infrastructure.Sql.Database;
+
+namespace AsianLines.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateDatabase();
+        }
+
+        private static void CreateDatabase()
+        {
+            var context = new AdminDbContext();
+            context.Database.Initialize(true);
+        }
+    }
+}
