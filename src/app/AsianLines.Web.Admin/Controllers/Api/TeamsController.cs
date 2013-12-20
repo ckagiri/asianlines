65cb97915fcd7080d10039fb0db4305bdf09dd86
ckagiri <charleskagiri@gmail.com>
2013-12-20 12:01:23 +0300

    added teams-api controller

---
+++ src/app/AsianLines.Web.Admin/Controllers/Api/TeamsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using AsianLines.Core.Model;
+using AsianLines.Infrastructure.Sql.Database;
+
+namespace AsianLines.Web.Admin.Controllers.Api
+{
+    public class TeamsController : ApiController
+    {
+        public IEnumerable<Team> Get()
+        {
+            var context = new AdminDbContext();
+            var teams = context.Teams.ToList();
+            return teams;
+        }
+    }
+}
