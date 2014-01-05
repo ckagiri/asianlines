15b23a657ad1549050de2ee0278dc5b9ad5f3a57
ckagiri <charleskagiri@gmail.com>
2014-01-05 15:35:43 +0300

    added required third party libraries

---
+++ src/app/Ligi.Web.Admin/App_Start/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Ligi.Web.Admin.Startup))]
+
+namespace Ligi.Web.Admin
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ src/app/Ligi.Web.Public/App_Start/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Ligi.Web.Public.Startup))]
+
+namespace Ligi.Web.Public
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
