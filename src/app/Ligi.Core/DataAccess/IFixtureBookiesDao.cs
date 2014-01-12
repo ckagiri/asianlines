f24e406649c423bdb26798a9d8010f7e525219d1
ckagiri <charleskagiri@gmail.com>
2014-01-12 09:47:07 +0300

    added betting process-managers

---
+++ src/app/Ligi.Core/DataAccess/IFixtureBookiesDao.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ligi.Core.DataAccess
+{
+    public interface IFixtureBookiesDao
+    {
+        List<Guid> GetBookies(Guid fixtureId);
+    }
+}
---
+++ src/app/Ligi.Core/Processes/BetProcessRouter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Ligi.Core.DataAccess;
+using Ligi.Core.Events;
+using Ligi.Core.Events.Domain;
+
+namespace Ligi.Core.Processes
+{
+    public class BetProcessRouter : 
+        IEventHandler<BetPlaced>,   
+        IEventHandler<MatchResultConfirmed>
+    {
+        private readonly Func<IProcessManagerDataContext<BetProcess>> _contextFactory;
+        private readonly IFixtureBookiesDao _fixtureBookiesDao;
+
+        public BetProcessRouter(
+            Func<IProcessManagerDataContext<BetProcess>> contextFactory, IFixtureBookiesDao fixtureBookiesDao)
+        {
+            _contextFactory = contextFactory;
+            _fixtureBookiesDao = fixtureBookiesDao;
+        }
+
+        public void Handle(BetPlaced @event)
+        {
+            using (var context = _contextFactory.Invoke())
+            {
+                var fixtureId = @event.Bet.FixtureId;
+                var p = context.Find(x => x.FixtureId == fixtureId);
+                if (p == null)
+                {
+                    p = new BetProcess(fixtureId);
+                }
+                p.Handle(@event);
+                context.Save(p);
+            }
+        }
+
+        public void Handle(MatchResultConfirmed @event)
+        {
+            using (var context = _contextFactory.Invoke())
+            {
+                var p = context.Find(x => x.FixtureId == @event.FixtureId);
+                if (p != null)
+                {
+                    p.Bookies = _fixtureBookiesDao.GetBookies(@event.FixtureId);
+                    p.Handle(@event);
+                    context.Save(p);
+                }
+            }
+        }
+    }
+}
