b40c9bff3d0deb820b4311c800d7e36a8c1893d5
ckagiri <charleskagiri@gmail.com>
2014-01-12 09:48:21 +0300

    added validation classes

---
+++ src/app/Ligi.Core/Validation/DomainValidationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ligi.Core.Validation
+{
+    public class DomainValidationException : Exception
+    {
+        public DomainValidationException(ValidationResultInfo validationResults)
+        {
+            ValidationResults = validationResults;
+        }
+
+        public ValidationResultInfo ValidationResults { get; set; }
+    }
+
+}
---
+++ src/app/Ligi.Core/Validation/ValidationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ligi.Core.Validation
+{
+    public static class ValidationExtensions
+    {
+        public static ValidationResultInfo BasicValidation<T>(this T itemToValidate)
+        {
+            var vc = new ValidationContext(itemToValidate, null, null);
+            var results = new List<ValidationResult>();
+            Validator.TryValidateObject(itemToValidate, vc, results, true);
+
+            return new ValidationResultInfo { Results = results };
+        }
+    }
+}
