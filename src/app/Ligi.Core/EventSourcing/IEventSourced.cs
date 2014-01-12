940a32970dfd6902965d3650751ccc81b8ae5d06
ckagiri <charleskagiri@gmail.com>
2014-01-12 09:16:42 +0300

    event sourcing core files

---
+++ src/app/Ligi.Core/EventSourcing/IEventSourced.cs
@@ -0,0 +1,13 @@
+﻿using Ligi.Core.Events;
+using System;
+using System.Collections.Generic;
+
+namespace Ligi.Core.EventSourcing
+{
+    public interface IEventSourced
+    {
+        Guid Id { get; }
+        int Version { get; }
+        IEnumerable<IVersionedEvent> Events { get; }
+    }
+}
