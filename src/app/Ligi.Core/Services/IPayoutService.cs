378f772b1bf69809f21c56b3d0da6e7daccfef42
ckagiri <charleskagiri@gmail.com>
2014-01-12 09:35:24 +0300

    added payout service

---
+++ src/app/Ligi.Core/Services/IPayoutService.cs
@@ -0,0 +1,15 @@
+﻿using Ligi.Core.Model;
+
+namespace Ligi.Core.Services
+{
+    public interface IPayoutService
+    {
+        PayoutResult GetPayout(Bet bet, int homeScore, int awayScore);
+    }
+
+    public class PayoutResult
+    {
+        public BetResult Result { get; set; }
+        public decimal Payout { get; set; }
+    }
+}
