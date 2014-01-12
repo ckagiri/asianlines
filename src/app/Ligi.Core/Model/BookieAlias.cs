9a1c85f29f46f653a92f9703c2aae3917fe77464
ckagiri <charleskagiri@gmail.com>
2014-01-12 09:34:51 +0300

    added bettor domain model

---
+++ src/app/Ligi.Core/Model/BookieAlias.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ligi.Core.Model
+{
+    public class BookieAlias
+    {
+        public Guid Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        //public int TransactionVersion { get; set; }
+    }
+}
