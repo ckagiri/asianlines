673069c8d59a36fd0aa9c68ad492f181c43c559e
ckagiri <charleskagiri@gmail.com>
2014-01-12 09:03:52 +0300

    added betting command contracts

---
+++ src/app/Ligi.Core/Commands/Contracts/TransactPayout.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Ligi.Core.Model;
+
+namespace Ligi.Core.Commands.Contracts
+{
+    public class TransactPayout : ICommand
+    {
+        public TransactPayout()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+        public Guid BookieId { get; set; }
+        public Guid FixtureId { get; set; }
+        public MatchStatus MatchStatus { get; set; }
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+    }
+}
---
+++ src/app/Ligi.Core/Commands/Contracts/UpdateSeasonAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Ligi.Core.Model;
+
+namespace Ligi.Core.Commands.Contracts
+{
+    public class UpdateSeasonAccount : ICommand
+    {
+        public UpdateSeasonAccount()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+        public Guid SeasonId { get; set; }
+        public Guid BettorId { get; set; }
+        public Guid BookieId { get; set; }
+        public List<BetTransaction> BetTransactions { get; set; }
+    }
+}
