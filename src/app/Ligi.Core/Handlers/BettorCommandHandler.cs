bd4415b2229b6349adaa0790dbaf7fee1c085b0c
ckagiri <charleskagiri@gmail.com>
2014-01-12 09:36:42 +0300

    bettor and bookie command handlers

---
+++ src/app/Ligi.Core/Handlers/BettorCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using Ligi.Core.Commands;
+using Ligi.Core.Commands.Domain;
+using Ligi.Core.EventSourcing;
+using Ligi.Core.Model;
+
+namespace Ligi.Core.Handlers
+{
+    public class BettorCommandHandler :
+        ICommandHandler<SubmitBetslip>,
+        ICommandHandler<UpdateSeasonAccount>
+    {
+        private readonly IEventSourcedRepository<Bettor> _repository;
+
+        public BettorCommandHandler(IEventSourcedRepository<Bettor> repository)
+        {
+            _repository = repository;
+        }
+
+        public void Handle(SubmitBetslip command)
+        {
+            var bettor = _repository.Find(command.UserId);
+            if (bettor == null)
+            {
+                bettor = new Bettor(command.UserId);
+            }
+
+            bettor.SubmitBetslip(command.UserName, command.SeasonId, command.Bets, command.TimeStamp);
+
+            _repository.Save(bettor, command.Id.ToString());
+        }
+
+        public void Handle(UpdateSeasonAccount command)
+        {
+            var bettor = _repository.Get(command.BettorId);
+            bettor.UpdateSeasonAccount(command.SeasonId, command.BookieId, command.BetTransactions);
+            _repository.Save(bettor, command.Id.ToString());
+        }
+    }
+}
---
+++ src/app/Ligi.Core/Handlers/BookieCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using Ligi.Core.Commands;
+using Ligi.Core.Commands.Domain;
+using Ligi.Core.EventSourcing;
+using Ligi.Core.Model;
+using Ligi.Core.Services;
+
+namespace Ligi.Core.Handlers
+{
+    public class BookieCommandHandler : 
+        ICommandHandler<ProcessBets>,
+        ICommandHandler<TransactPayout>
+    {
+        private readonly IEventSourcedRepository<Bookie> _repository;
+        private readonly IPayoutService _payoutService;
+
+        public BookieCommandHandler(IEventSourcedRepository<Bookie> repository, IPayoutService payoutService)
+        {
+            _repository = repository;
+            _payoutService = payoutService;
+        }
+
+        public void Handle(ProcessBets command)
+        {
+            var bookie = _repository.Find(command.BookieId);
+            if (bookie == null)
+            {
+                bookie = new Bookie(command.BookieId, command.StartDate, command.EndDate, command.UserId, command.SeasonId);
+            }
+            bookie.ProcessBets(command.BetslipId, command.UserName, command.Bets);
+            _repository.Save(bookie, command.Id.ToString());
+        }
+
+        public void Handle(TransactPayout command)
+        {
+            var bookie = _repository.Get(command.BookieId);
+            bookie.TransactPayout(command.FixtureId, command.MatchStatus, command.HomeScore, command.AwayScore, _payoutService);
+            _repository.Save(bookie, command.Id.ToString());
+        }
+    }
+}
