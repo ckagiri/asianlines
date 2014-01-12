91394fe8dd6c45a85a0be89960d553c0fcf88278
ckagiri <charleskagiri@gmail.com>
2014-01-12 09:36:15 +0300

    bookies by fixture

---
+++ src/app/Ligi.Core/Model/FixtureBookieIndex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Ligi.Core.DomainBase;
+
+namespace Ligi.Core.Model
+{
+    // BookiesByFixture
+    public class FixtureBookieIndex : IAggregateRoot
+    {
+        public Guid Id { get; private set; }
+        public Guid FixtureId { get; set; }
+        public Guid BookieId { get; set; }
+
+        protected FixtureBookieIndex()
+        { }
+
+        public FixtureBookieIndex(Guid id, Guid fixtureId, Guid bookieId)
+        {
+            Id = id;
+            FixtureId = fixtureId;
+            BookieId = bookieId;
+        }
+    }
+}
