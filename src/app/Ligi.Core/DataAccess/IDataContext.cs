f284c192143d6ac7a0a3ad7a61d0be76514dbdff
ckagiri <charleskagiri@gmail.com>
2014-01-12 09:41:01 +0300

    added data-context contract

---
+++ src/app/Ligi.Core/DataAccess/IDataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+using Ligi.Core.DomainBase;
+
+namespace Ligi.Core.DataAccess
+{
+    public interface IDataContext<T> : IDisposable
+        where T : IAggregateRoot
+    {
+        T Find(Guid id);
+        T Find(Expression<Func<T, bool>> predicate);
+        void Save(T aggregate);
+    }
+}
