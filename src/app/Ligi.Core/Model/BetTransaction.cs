289d1c3529b809d7f2082ddead9b5a2ea9f679d4
ckagiri <charleskagiri@gmail.com>
2014-01-12 09:01:08 +0300

    added bet objects and accounts models

---
+++ src/app/Ligi.Core/Model/BetTransaction.cs
@@ -0,0 +1,15 @@
+﻿namespace Ligi.Core.Model
+{
+    public class BetTransaction
+    {
+        public TransactionType TxType { get; set; }
+        public Bet Bet { get; set; }
+    }
+
+    public enum TransactionType
+    {
+        New,
+        Update,
+        Payout
+    }
+}
---
+++ src/app/Ligi.Core/Model/MonthAccount.cs
@@ -0,0 +1,8 @@
+﻿namespace Ligi.Core.Model
+{
+    public class MonthAccount : SeasonAccount
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+    }
+}
