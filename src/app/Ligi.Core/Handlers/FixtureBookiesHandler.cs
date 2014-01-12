f00750842b06fc430348c945c160fd4a2f064959
ckagiri <charleskagiri@gmail.com>
2014-01-12 09:41:21 +0300

    fixture bookies handler

---
+++ src/app/Ligi.Core/Handlers/FixtureBookiesHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using Ligi.Core.Commands;
+using Ligi.Core.Commands.Domain;
+using Ligi.Core.DataAccess;
+using Ligi.Core.Model;
+
+namespace Ligi.Core.Handlers
+{
+    public class FixtureBookiesHandler : ICommandHandler<AddBookie>
+    {
+        private readonly Func<IDataContext<FixtureBookieIndex>> _contextFactory;
+
+        public FixtureBookiesHandler(Func<IDataContext<FixtureBookieIndex>> contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public void Handle(AddBookie command)
+        {
+            var repository = _contextFactory();
+            using (repository)
+            {
+                var fixtureBookieIndex = repository.Find(i =>
+                    i.FixtureId == command.FixtureId && i.BookieId == command.BookieId);
+
+                if (fixtureBookieIndex == null)
+                {
+                    fixtureBookieIndex = new FixtureBookieIndex(command.Id, command.FixtureId, command.BookieId);
+                    repository.Save(fixtureBookieIndex);
+                }
+                else
+                {
+                    Trace.TraceError("Ignoring command {0} because bookie was already added.", command.Id);
+                }
+            }
+        }
+    }
+}
