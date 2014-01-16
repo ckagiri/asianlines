7440d0ec26229bb425860c058c4ed89400cabeaf
ckagiri <charleskagiri@gmail.com>
2014-01-16 21:23:41 +0300

    dbcontexts - different contexts

---
+++ src/app/Ligi.Infrastructure.Sql/Database/IndexDictionaryDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using Ligi.Core.Model;
+
+namespace Ligi.Infrastructure.Sql.Database
+{
+    public class IndexDictionaryDbContext : DbContext
+    {
+        //public const string SchemaName = "IndexDictionaries";
+
+        public IndexDictionaryDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<FixtureBookieIndex>().ToTable("FixtureBookies");
+        }
+
+        public T Find<T>(Guid id) where T : class
+        {
+            return Set<T>().Find(id);
+        }
+
+        public IQueryable<T> Query<T>() where T : class
+        {
+            return Set<T>();
+        }
+    }
+}
