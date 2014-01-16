1783a77d58efa07aaa959a11a71294183eabece4
ckagiri <charleskagiri@gmail.com>
2014-01-16 21:00:43 +0300

    league and season validators - onsave

---
+++ src/app/Ligi.Infrastructure.Sql/Validation/IValidator.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using Ligi.Core.Validation;
+
+namespace Ligi.Infrastructure.Sql.Validation
+{
+    public interface IValidator<in T> where T : class
+    {
+        ValidationResultInfo Validate(T entity, DbContext context);
+    }
+}
