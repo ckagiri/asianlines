80f939086d5437c12e0e58a51bd348300f407df1
ckagiri <charleskagiri@gmail.com>
2014-01-16 17:23:40 +0300

    tests - command/event domain-processes routing

---
+++ src/test/Ligi.Core.Tests/BetProcessFixture.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Ligi.Core.Commands.Domain;
+using Ligi.Core.Events.Domain;
+using Ligi.Core.Model;
+using Ligi.Core.Processes;
+using Xunit;
+
+namespace Ligi.Core.Tests
+{
+    public class BetContext
+    {
+        protected BetProcess _sut;
+        protected static Guid fixtureId = Guid.NewGuid();
+
+        public BetContext()
+        {
+            _sut = new BetProcess(fixtureId);
+        }
+    }
+
+    public class when_bet_is_placed : BetContext
+    {
+        private BetPlaced _betPlaced;
+        private Bet _bet1 = new Bet
+        {
+            FixtureId = fixtureId,
+            BetType = BetType.AsianGoals,
+            BetPick = BetPick.Away,
+            Handicap = 1.5m,
+            Wager = 200,
+            Stake = 200,
+            TimeStamp = DateTime.Now
+        };
+
+        public when_bet_is_placed()
+        {
+            _betPlaced = new BetPlaced
+                             {
+                                 SourceId = Guid.NewGuid(),
+                                 UserId = Guid.NewGuid(),
+                                 SeasonId = Guid.NewGuid(),
+                                 Bet = _bet1
+                             };
+
+            _sut.Handle(_betPlaced);
+        }
+
+        [Fact]
+        public void then_sends_add_bookie_command()
+        {
+            var addIndex = _sut.Commands.Select(x => x.Body).OfType<AddBookie>().Single();
+
+            Assert.Equal(_betPlaced.SourceId, addIndex.BookieId);
+            Assert.Equal(_sut.FixtureId,  addIndex.FixtureId);
+        }
+    }
+}
---
+++ src/test/Ligi.Core.Tests/BookieProcessFixture.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ligi.Core.Commands.Domain;
+using Ligi.Core.Events.Domain;
+using Ligi.Core.Model;
+using Ligi.Core.Processes;
+using Ligi.Core.Utils;
+using Xunit;
+
+namespace Ligi.Core.Tests
+{
+    namespace given_uninitialized_process
+    {
+        public class Context
+        {
+            protected BookieProcess _sut;
+            public Context()
+            {
+                _sut = new BookieProcess();
+            }
+        }
+
+        public class when_betslip_is_submitted : Context
+        {
+            private BetslipSubmitted _betslipSubmitted;
+            private static readonly DateTime Now = DateTime.Now;
+            private Bet _bet1 = new Bet
+                                    {
+                                        FixtureId = Guid.NewGuid(),
+                                        BetType = BetType.AsianHandicap,
+                                        BetPick = BetPick.Away,
+                                        Handicap = 1.5m,
+                                        Wager = 200,
+                                        TimeStamp = Now
+                                    };
+            private Bet _bet2 = new Bet
+                                    {
+                                        FixtureId = Guid.NewGuid(),
+                                        BetType = BetType.AsianGoals,
+                                        BetPick = BetPick.Over,
+                                        Handicap = 1.5m,
+                                        Wager = 200,
+                                        TimeStamp = Now
+                                    };
+            public when_betslip_is_submitted()
+            {
+                _betslipSubmitted = new BetslipSubmitted
+                                        {
+                                            SourceId = Guid.NewGuid(),
+                                            BetslipId = Guid.NewGuid(),
+                                            SeasonId = Guid.NewGuid(),
+                                            BookieId = Guid.NewGuid(),
+                                            StartDate = new MonToSunWeek().FirstDayOfWeek(),
+                                            EndDate = new MonToSunWeek().LastDayOfWeek(),
+                                            Bets = new List<Bet>
+                                                       {
+                                                           _bet1,
+                                                           _bet2
+                                                       }
+                                        };
+                _sut.Handle(_betslipSubmitted);
+            }
+
+            [Fact]
+            public void then_sends_process_bets_command()
+            {
+                Assert.Equal(1, _sut.Commands.Count());
+            }
+
+            [Fact]
+            public void then_process_bets_is_sent_for_specific_betslip()
+            {
+                var betslipSubmittion = _sut.Commands.Select(x => x.Body).OfType<ProcessBets>().Single();
+                Assert.Equal(_betslipSubmitted.BetslipId, betslipSubmittion.BetslipId);
+            }
+
+            [Fact]
+            public void then_transitions_to_running_state()
+            {
+                Assert.Equal(BookieProcess.ProcessState.Running, _sut.State);
+            }
+        }
+    }
+
+    namespace given_process_running
+    {
+        public class Context
+        {
+            protected BookieProcess _sut;
+            protected Guid BettorId;
+            protected Guid SeasonId;
+            protected Guid BookieId;
+            protected Bet Bet1;
+            protected Bet Bet2;
+            protected Guid Id;
+            protected Guid FixtureId1;
+            protected Guid FixtureId2;
+            protected static DateTime Now = DateTime.Now;
+            public Context()
+            {
+                _sut = new BookieProcess();
+                BettorId = Guid.NewGuid();
+                SeasonId = Guid.NewGuid();
+                BookieId = Guid.NewGuid();
+                FixtureId1 = Guid.NewGuid();
+                FixtureId2 = Guid.NewGuid();
+                Bet1 = new Bet
+                           {
+                               FixtureId = FixtureId1,
+                               BetType = BetType.AsianHandicap,
+                               BetPick = BetPick.Away,
+                               Handicap = 1.5m,
+                               Wager = 200,
+                               TimeStamp = Now
+                           };
+                Bet2 = new Bet
+                           {
+                               FixtureId = FixtureId2,
+                               BetType = BetType.AsianGoals,
+                               BetPick = BetPick.Over,
+                               Handicap = 1.5m,
+                               Wager = 200,
+                               TimeStamp = Now
+                           };
+
+                _sut.Handle(new BetslipSubmitted
+                                {
+                                    SourceId = BettorId,
+                                    BetslipId = Guid.NewGuid(),
+                                    SeasonId = SeasonId,
+                                    BookieId = BookieId,
+                                    StartDate = new MonToSunWeek().FirstDayOfWeek(),
+                                    EndDate = new MonToSunWeek().LastDayOfWeek(),
+                                    Bets = new List<Bet>
+                                               {
+                                                   Bet1,
+                                                   Bet2
+                                               }
+                                });
+                Bet1.Stake = Bet1.Wager;
+                Bet2.Stake = Bet2.Wager;
+                _sut.Handle(new BetsProcessed
+                                {
+                                    SourceId = BettorId,
+                                    BetslipId = Guid.NewGuid(),
+                                    BetTransactions = new List<BetTransaction>
+                                                          {
+                                                              new BetTransaction
+                                                                  {
+                                                                      TxType = TransactionType.New,
+                                                                      Bet = Bet1
+                                                                  },
+                                                              new BetTransaction
+                                                                  {
+                                                                      TxType = TransactionType.New,
+                                                                      Bet = Bet2
+                                                                  }
+                                                          }
+                                });
+
+                // season acc updtd
+
+            }
+
+
+            [Fact]
+            public void then_transitions_state()
+            {
+                Assert.Equal(BookieProcess.ProcessState.AwaitingSeasonAccountAck, _sut.State);
+            }
+        }
+    }
+}
