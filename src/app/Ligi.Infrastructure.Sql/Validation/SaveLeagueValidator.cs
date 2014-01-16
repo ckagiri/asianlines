ce317bce023e77c1850fdfd860ff92f686b9aae5
ckagiri <charleskagiri@gmail.com>
2014-01-16 21:00:43 +0300

    league and season validators - onsave

---
+++ src/app/Ligi.Infrastructure.Sql/Validation/SaveLeagueValidator.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using Ligi.Core.Model;
+using Ligi.Core.Validation;
+
+namespace Ligi.Infrastructure.Sql.Validation
+{
+    public class SaveLeagueValidator : IValidator<League>
+    {
+        public ValidationResultInfo Validate(League entity, DbContext context)
+        {
+            ValidationResultInfo vri = entity.BasicValidation();
+            if (vri.Results.Any()) return vri;
+
+            if (context.Set<League>()
+                .Where(l => l.Id != entity.Id)
+                .Any(l => l.Code == entity.Code))
+            {
+                vri.Results.Add(new ValidationResult("Duplicate league code."));
+                return vri;
+            }
+
+            if (context.Set<League>()
+                .Where(l => l.Id != entity.Id)
+                .Any(l => l.Name == entity.Name))
+                vri.Results.Add(new ValidationResult("Duplicate league name."));
+            return vri;
+        }
+    }
+}
---
+++ src/app/Ligi.Infrastructure.Sql/Validation/SaveSeasonValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using Ligi.Core.Model;
+using Ligi.Core.Validation;
+
+namespace Ligi.Infrastructure.Sql.Validation
+{
+    public class SaveSeasonValidator : IValidator<Season>
+    {
+        public ValidationResultInfo Validate(Season entity, DbContext context)
+        {
+            ValidationResultInfo vri = entity.BasicValidation();
+            if (vri.Results.Any()) return vri;
+
+            if (entity.LeagueId == Guid.Empty)
+            {
+                vri.Results.Add(new ValidationResult("League is required."));
+                return vri;
+            }
+            return vri;
+        }
+    }
+}
