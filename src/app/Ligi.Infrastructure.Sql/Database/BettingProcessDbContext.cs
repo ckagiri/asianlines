1d006ed09d01d2f52a1f19f9ed54df234ff2c089
ckagiri <charleskagiri@gmail.com>
2014-01-16 21:23:41 +0300

    dbcontexts - different contexts

---
+++ src/app/Ligi.Infrastructure.Sql/Database/BettingProcessDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Entity;
+using Ligi.Core.Processes;
+using Ligi.Infrastructure.Sql.Processes;
+
+namespace Ligi.Infrastructure.Sql.Database
+{
+    public class BettingProcessDbContext : DbContext
+    {
+        public BettingProcessDbContext(string nameOrConnectionString) 
+            : base(nameOrConnectionString)
+        { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<BookieProcess>().ToTable("BookieProcess");
+            modelBuilder.Entity<BetProcess>().ToTable("BetProcess");
+
+        }
+
+        // Define the available entity sets for the database.
+        public DbSet<BookieProcess> BookieProcesses { get; set; }
+        public DbSet<BetProcess> BetProcesses { get; set; }
+
+        // Table for pending undispatched messages associated with a process manager.
+        public DbSet<UndispatchedMessages> UndispatchedMessages { get; set; }
+
+
+    }
+}
---
+++ src/app/Ligi.Infrastructure.Sql/Database/SqlDataContext.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using Ligi.Core.DataAccess;
+using Ligi.Core.DomainBase;
+using Ligi.Core.Messaging;
+using System;
+using System.Data.Entity;
+
+namespace Ligi.Infrastructure.Sql.Database
+{
+    public class SqlDataContext<T> : IDataContext<T> where T : class, IAggregateRoot
+    {
+        private readonly IEventBus _eventBus;
+        private readonly DbContext _context;
+
+        public SqlDataContext(Func<DbContext> contextFactory, IEventBus eventBus)
+        {
+            _eventBus = eventBus;
+            _context = contextFactory.Invoke();
+        }
+
+        public T Find(Guid id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public T Find(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().FirstOrDefault(predicate);
+        }
+
+        public void Save(T aggregateRoot)
+        {
+            var entry = _context.Entry(aggregateRoot);
+
+            if (entry.State == EntityState.Detached)
+                _context.Set<T>().Add(aggregateRoot);
+
+            // Can't have transactions across storage and message bus.
+            _context.SaveChanges();
+
+            var eventPublisher = aggregateRoot as IEventPublisher;
+            if (eventPublisher != null)
+                _eventBus.Publish(eventPublisher.Events);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~SqlDataContext()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+        }
+    }
+}
