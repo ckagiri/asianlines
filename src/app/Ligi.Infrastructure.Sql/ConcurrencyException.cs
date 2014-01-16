068deccd210f787f51fc7c441dc8b1778ed1640d
ckagiri <charleskagiri@gmail.com>
2014-01-16 17:49:23 +0300

    sql event store

---
+++ src/app/Ligi.Infrastructure.Sql/ConcurrencyException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Ligi.Infrastructure.Sql
+{
+    [Serializable]
+    public class ConcurrencyException : Exception
+    {
+        public ConcurrencyException() { }
+
+        public ConcurrencyException(string message) : base(message) { }
+
+        public ConcurrencyException(string message, Exception inner) : base(message, inner) { }
+
+        protected ConcurrencyException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ src/app/Ligi.Infrastructure.Sql/Serialization/TextSerializerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace Ligi.Infrastructure.Sql.Serialization
+{
+    public static class TextSerializerExtensions
+    {
+        public static string Serialize<T>(this ITextSerializer serializer, T data)
+        {
+            using (var writer = new StringWriter())
+            {
+                serializer.Serialize(writer, data);
+                return writer.ToString();
+            }
+        }
+
+        public static T Deserialize<T>(this ITextSerializer serializer, string serialized)
+        {
+            using (var reader = new StringReader(serialized))
+            {
+                return (T)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
